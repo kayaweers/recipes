2c39ae1a2ef1061ea583278407433490c0c8db73
Kaya Weers <kaya@weers.net>
2024-10-21 15:29:21 +0200

    fix dependency

--- src/main/java/com/kayaweers/recipes/RecipesApplication.java
+++ src/main/java/com/kayaweers/recipes/RecipesApplication.java
@@ -2,9 +2,7 @@ package com.kayaweers.recipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class RecipesApplication {
 
